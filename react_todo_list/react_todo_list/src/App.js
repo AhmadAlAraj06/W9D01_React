d696af2ca0b99ab7f7b2e3e4a39544311cfb3d3d
AhmadAlAraj06 <81652458+AhmadAlAraj06@users.noreply.github.com>
2021-05-24 01:05:11 +0300

    step 6 finished

--- react_todo_list/react_todo_list/src/App.js
+++ react_todo_list/react_todo_list/src/App.js
@@ -5,7 +5,10 @@ import ListItem from "./components/ListItem";
 
 
 const App = () => {
+ const todos=[{todo: "wake up", id: 1}, {todo: "breakfast", id: 2}, {todo: "study", id: 3}]
   return <div><h1>Todo List</h1></div>
-  
 }
+
 export default App;
+
+
